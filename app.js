c49078ebac7862b1fffda00b3a8330dc7579826f
elsammio <109492335+elsammio@users.noreply.github.com>
2024-02-25 23:29:04 -0500

    versión final juego numero secreto

---
+++ app.js
@@ -0,0 +1,81 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados =[];
+let numeroMaximo = 10;
+console.log(numeroSecreto);
+
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    
+    console.log(intentos);
+    if(numeroDeUsuario === numeroSecreto){
+
+        asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    }
+    else if(numeroDeUsuario > numeroSecreto){
+        asignarTextoElemento('p','Numero secreto es menor');
+    }else{
+        asignarTextoElemento('p','Numero secreto es mayor');
+    }
+    intentos++;
+    limpiarCaja();
+    return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+    
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
+
+    }else{
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        }else{
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+
+    }
+    
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+    asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de numeros
+    condicionesIniciales(); 
+    //Gnerar numero aleatorio
+    
+    //inicializar el numero de intentos
+    //Deshabiliatr el botob de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+    
+}
+
+condicionesIniciales(); 
